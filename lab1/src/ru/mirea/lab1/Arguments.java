bcf0c47628d7c38a43dca7392fec3c6d186b0e90
Ярослав Стасишин <SlRocker@yandex.ru>
2021-10-22 00:22:59 +0300

    Add files via upload

---
+++ lab1/src/ru/mirea/lab1/Arguments.java
@@ -0,0 +1,10 @@
+package ru.mirea.lab1;
+
+import java.util.Scanner;
+
+public class Arguments {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.println(input.nextLine());
+    }
+}
